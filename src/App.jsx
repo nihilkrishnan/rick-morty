f28e0fa78ef55a538ba2b4f55a92794afe7343a3
nihilkrish <nihilkrish27@gmail.com>
2024-07-22 20:47:53 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CharacterList from './components/CharacterList'
+
+
+
+const App = () => {
+  console.log('Rendering App')
+  return (
+    <div className="App">
+      <CharacterList/>
+    </div>
+  )
+}
+
+export default App;
